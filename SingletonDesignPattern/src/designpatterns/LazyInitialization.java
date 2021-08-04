022b32559ef5599e989ace0c86986bb8e820b9f3
= <logkashunmughum@gmail.com>
2021-08-05 00:54:10 +0100

    Included code for lazy initialization

---
+++ SingletonDesignPattern/src/designpatterns/LazyInitialization.java
@@ -0,0 +1,31 @@
+package designpatterns;
+
+public class LazyInitialization {
+
+	private static LazyInitialization instance;
+	
+	private LazyInitialization() {
+		
+	}
+	
+	public static LazyInitialization getInstance() {
+		if(instance == null) {
+			instance = new LazyInitialization();
+		}
+		return instance;
+	}
+	public static void main(String[] args) {
+		
+		LazyInitialization instance1 = getInstance();
+		System.out.println("Object ID is : " + instance1);
+		
+		LazyInitialization instance2 = getInstance();
+		System.out.println("Object ID is : " + instance2);
+		
+		if(instance1 == instance2) {
+			System.out.println("Singleton pattern using Lazy initialization verified successfully");
+		}
+
+	}
+
+}
